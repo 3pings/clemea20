e6a6dfc587e795219b05442312be8b68adb52f32
jbarksdale@threepings.com <jbarksdale@threepings.com>
2020-01-08 16:38:44 -0500

    modified bash file

--- 01-livewall/destroy-livewall.sh
+++ 01-livewall/destroy-livewall.sh
@@ -5,8 +5,8 @@ kubectl delete -f yaml/apiserver-service.yaml
 kubectl delete -f yaml/frontend-service.yaml
 
 # Deployments
-kubectl delete -f yaml/weather-deployment.yaml
-kubectl delete -f yaml/event-deployment.yaml
-kubectl delete -f yaml/incident-deployment.yaml
-kubectl delete -f yaml/apiserver-deployment.yaml
-kubectl delete -f yaml/frontend-deployment.yaml
+kubectl delete -f yaml/weather-deployment-ready.yaml
+kubectl delete -f yaml/event-deployment-ready.yaml
+kubectl delete -f yaml/incident-deployment-ready.yaml
+kubectl delete -f yaml/apiserver-deployment-ready.yaml
+kubectl delete -f yaml/frontend-deployment-ready.yaml
